f266ee154476a40d5c5cf266c29e42db6e47eccb
CMWZ97I <syncpages@gmail.com>
2015-10-15 01:09:31 +0200

    Create example project

---
+++ maven/src/main/java/j2ee/mvn/App.java
@@ -0,0 +1,13 @@
+package j2ee.mvn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
