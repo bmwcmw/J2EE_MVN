27a5b1fe218dbd92d04233a20c2a07798df18218
CMWZ97I <syncpages@gmail.com>
2016-09-24 03:34:38 +0200

    Add xsd schema and info

--- javaee6/javaee6-test-war-module/src/main/java/j2ee/mvn/war/c/amazon2/package-info.java
+++ javaee6/javaee6-test-war-module/src/main/java/j2ee/mvn/war/c/amazon2/package-info.java
@@ -1,4 +1,9 @@
 /**
  * Example 2 using xjc-generated class from xsd schema, and JAXB mapping
+ * 
+ * http://webservices.amazon.com/AWSECommerceService/AWSECommerceService.xsd
+ * 
+ * launch in src\main\java\ :
+ * xjc -p j2ee.mvn.war.c.amazon2 j2ee\mvn\war\c\amazon2\xsd\AWSECommerceService.xsd
  */
 package j2ee.mvn.war.c.amazon2;
