2830857cc7442c6a067dd7afdd7aafef049e1c02
CMWZ97I <syncpages@gmail.com>
2016-09-26 00:39:55 +0200

    Preparing wadl client for prediction3

---
+++ javaee6/src/main/java/j2ee/mvn/war/c/prediction3/package-info.java
@@ -0,0 +1,18 @@
+/**
+ * WADL2JAVBA generated client
+ * 
+ * As an example, recall the predictions3JAX-RS service from Chapter 2whose base URL is:
+ * http://localhost:8080/predictions3/resourcesP/
+ * 
+ * The WADL document for this service is available under Jersey with the URL:
+ * http://localhost:8080/predictions3/resourcesP/application.wadl
+ * 
+ * If the implementation were Apache CXF instead of Jersey, the URL would be:
+ * http://localhost:8080/predictions3/resourcesP?wadl
+ * 
+ * Then run : 
+ * wadl2java -o outputDir -p package [-a] [-s jaxrs20] [-c customization]* file.wadl
+ *  * (in wadl-dist-1.1.6\bin) : 
+ * wadl2java -o ..\..\src\main\java\j2ee\mvn\war\c\prediction3 -p j2ee.mvn.war.c.prediction3 ..\..\src\main\java\j2ee\mvn\war\c\prediction3\prediction3.wadl
+ */
+package j2ee.mvn.war.c.prediction3;
