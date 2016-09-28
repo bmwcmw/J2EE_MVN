d5fd2a5def58e2c2584b82b25529e8b4e6c85a6e
CMWZ97I <syncpages@gmail.com>
2016-09-29 01:12:59 +0200

    Adage module ok

--- javaee6/javaee6-war-b-adage/src/main/java/j2ee/mvn/war/b/adage/AdagesPublisher.java
+++ javaee6/javaee6-war-b-adage/src/main/java/j2ee/mvn/war/b/adage/AdagesPublisher.java
@@ -9,8 +9,8 @@ import com.sun.net.httpserver.HttpServer;
 
 /**
  * com.sun.net.httpserver is indicated as non resolved due to an access
- * restriction, add an access rule "Accessible" for the pattern
- * com/sun/net/httpserver/**
+ * restriction, add an access rule "Accessible" in Java Build Path ==> JRE System Library
+ * for the pattern com/sun/net/httpserver/**
  * 
  * http://stackoverflow.com/questions/9266632/access-restriction-is-not-
  * accessible-due-to-restriction-on-required-library/10642163#10642163
