7cb15c0236af7e87c257d3d8422d9b8f393aacb1
CMWZ97I <syncpages@gmail.com>
2016-10-03 01:30:59 +0200

    Add Jersey generated wadl file but error in client class generation

--- javaee6/src/main/java/j2ee/mvn/war/c/prediction3/package-info.java
+++ javaee6/src/main/java/j2ee/mvn/war/c/prediction3/package-info.java
@@ -12,7 +12,17 @@
  * 
  * Then run : 
  * wadl2java -o outputDir -p package [-a] [-s jaxrs20] [-c customization]* file.wadl
- *  * (in wadl-dist-1.1.6\bin) : 
- * wadl2java -o ..\..\src\main\java\j2ee\mvn\war\c\prediction3 -p j2ee.mvn.war.c.prediction3 ..\..\src\main\java\j2ee\mvn\war\c\prediction3\prediction3.wadl
+ * 
+ * (run in wadl-dist-1.1.6\bin) : 
+ * wadl2java -o ..\..\src\main\java\j2ee\mvn\war\c\prediction3 -p j2ee.mvn.war.c.prediction3 ..\..\src\main\java\j2ee\mvn\war\c\prediction3\application.wadl
+ * 
+ * FIEME : 
+ * java.net.URISyntaxException: Illegal character in path at index 2: ..\..\src\main\java\j2ee\mvn\war\c\prediction3\application.wadl
+        at java.net.URI$Parser.fail(URI.java:2829)
+        at java.net.URI$Parser.checkChars(URI.java:3002)
+        at java.net.URI$Parser.parseHierarchical(URI.java:3086)
+        at java.net.URI$Parser.parse(URI.java:3044)
+        at java.net.URI.<init>(URI.java:595)
+        at org.jvnet.ws.wadl2java.Main.main(Main.java:111)
  */
 package j2ee.mvn.war.c.prediction3;
