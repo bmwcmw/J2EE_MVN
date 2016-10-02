cc304c2beccfdb37aa8892329ab13cdbd44b74da
CMWZ97I <syncpages@gmail.com>
2016-10-03 01:55:38 +0200

    Add jersey client with compile dependency

---
+++ javaee6/src/main/java/j2ee/mvn/war/c/prediction2/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ * JAX-RS, Restlet, and the JAX-WS @WebServiceProviderframeworks for RESTful
+ * serv‐ ices include client-side APIs. These APIs are meant to simplify the
+ * task of writing RESTful clients in general, not just clients against a
+ * RESTful service implemented in a particular framework.
+ * 
+ * To underscore the point, this section takes a look at the JAX-RS client-side
+ * API (see Example 3-18); the client, however, goes against the servlet-based
+ * predictions2RESTful service from Chapter 2.
+ */
+package j2ee.mvn.war.c.prediction2;
